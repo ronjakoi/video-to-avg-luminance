89cd1a2e486b2574ea450154fe7b42cbce36ed51
Ronja Koistinen <ronja@tuxera.com>
2023-02-20 20:53:23 +0200

    Make it prettier and shut up pylint

--- vid2meanluminance.py
+++ vid2meanluminance.py
@@ -1,28 +1,45 @@
 #!/usr/bin/env python3
 
-import numpy as np
-import cv2 as cv
+"""
+This script calculates the mean relative luminance for every frame
+of a video file. It prints a floating point value for one frame per line.
+
+Ronja Koistinen <ronja@tuxera.com>
+"""
+
+# pylint: disable=c-extension-no-member
+# pylint: disable=missing-function-docstring
+
 import argparse
+import sys
+import cv2 as cv
 
-parser = argparse.ArgumentParser(description='Calculates the mean relative luminance for every frame of a video')
-parser.add_argument('video', type=str, help='Path to video file')
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(
+        description=__doc__
+    )
 
-cap = cv.VideoCapture(args.video)
+    parser.add_argument('video', type=str, help='Path to video file')
+    args = parser.parse_args()
 
-frames_read = 0
+    cap = cv.VideoCapture(args.video)
 
-while(True):
-    ret, frame = cap.read()
-    if not ret:
-        if frames_read == 0:
-            eprint("Zero frames read! Is this file broken?")
-        break
-    frames_read += 1
+    frames_read = 0
 
-    # there is also cv.COLOR_RGB2GRAY which possibly does the same thing,
-    # but it's difficult to tell from the documentation
-    lab = cv.cvtColor(frame, cv.COLOR_RGB2LAB)
-    luminance, _, _ = cv.split(lab)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            if frames_read == 0:
+                print("Zero frames read! Is this file broken?", file=sys.stderr)
+            break
+        frames_read += 1
 
-    print(luminance.mean())
+        # there is also cv.COLOR_RGB2GRAY which possibly does the same thing,
+        # but it's difficult to tell from the documentation
+        lab = cv.cvtColor(frame, cv.COLOR_RGB2LAB)
+        luminance, _, _ = cv.split(lab)
+
+        print(luminance.mean())
+
+if __name__ == "__main__":
+    main()
