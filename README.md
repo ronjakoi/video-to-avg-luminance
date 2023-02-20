dec3334bd38bd8ea05a97bd54265fd1f96f160c3
Ronja Koistinen <ronja@tuxera.com>
2023-02-20 16:48:17 +0200

    It works

---
+++ README.md
@@ -0,0 +1,18 @@
+# Video to average luminance
+
+Ronja Koistinen <ronja@tuxera.com>
+
+This script uses the OpenCV library to calculate the average
+[relative luminance](https://en.wikipedia.org/wiki/Relative_luminance)
+for each frame in a video file.
+
+## Dependencies
+
+* numpy
+* opencv
+
+## Usage
+
+```bash
+./vi2avgluminance.py myvideo.avi
+```
---
+++ vid2avgluminance.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import cv2 as cv
+import argparse
+
+parser = argparse.ArgumentParser(description='Calculates average relative luminance for every frame of a video')
+parser.add_argument('video', type=str, help='Path to image file')
+args = parser.parse_args()
+
+cap = cv.VideoCapture(args.video)
+
+frames_read = 0
+
+while(True):
+    ret, frame = cap.read()
+    if not ret:
+        if frames_read == 0:
+            eprint("Zero frames read! Is this file broken?")
+        break
+    frames_read += 1
+
+    # there is also cv.COLOR_RGB2GRAY which possibly does the same thing,
+    # but it's difficult to tell from the documentation
+    lab = cv.cvtColor(frame, cv.COLOR_RGB2LAB)
+    luminance, _, _ = cv.split(lab)
+
+    print(luminance.mean())
